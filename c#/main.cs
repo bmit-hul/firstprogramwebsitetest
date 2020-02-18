65a94134b8717d60d74b3c3253f7b8f8b396de2e
bmit-hul <loristhierryhuetter@gmail.com>
2020-02-18 10:09:58 +0100

    some code

---
+++ c#/main.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace DotnetBot {
+    public static class main {
+        public static void Main(string[] args){
+            string message = "Hello, World!";
+            Console.WriteLine(message);
+        }
+    }
+}
